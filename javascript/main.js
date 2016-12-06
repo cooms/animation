ecfe0f9300ae36b2aa785f556aa9b446585bd9f1
cooms <thcoomer@hotmail.com>
2016-12-06 15:31:03 +1300

    javascript with keyframes initiated

--- javascript/main.js
+++ javascript/main.js
@@ -1,41 +1,36 @@
 // javascript
-
 function bounce() {
 	document.getElementById('ball').style.backgroundColor = "blue";
 }
 
-// var style = document.createElement('style');
-// style.type = 'text/css';
-// style.innerHTML = 'body {}';
-// document.getElementById('ball').appendChild(style);
-// this.stylesheet = document.styleSheets[document.styleSheets.length-1];
-// try {
-//   this.stylesheet.insertRule('\
-// @keyframes bounce {
-// 		from, 20%, 53%, 80%, to {
-// 			-webkit-animation-timing-function: cubic-bezier(0.215, 0.610, 0.355, 1.000);
-// 			animation-timing-function: cubic-bezier(0.215, 0.610, 0.355, 1.000);
-// 			-webkit-transform: translate3d(0,0,0);
-// 			transform: translate3d(0,0,0);
-// 		}
+var cssRule;
 
-// 		40%, 43% {
-// 			-webkit-animation-timing-function: cubic-bezier(0.755, 0.050, 0.855, 0.060);
-// 			animation-timing-function: cubic-bezier(0.755, 0.050, 0.855, 0.060);
-// 			-webkit-transform: translate3d(0, -30px, 0);
-// 			transform: translate3d(0, -30px, 0);
-// 		}
+  // Returns a reference to the specified CSS rule(s).
 
-// 		70% {
-// 			-webkit-animation-timing-function: cubic-bezier(0.755, 0.050, 0.855, 0.060);
-// 			animation-timing-function: cubic-bezier(0.755, 0.050, 0.855, 0.060);
-// 			-webkit-transform: translate3d(0, -15px, 0);
-// 			transform: translate3d(0, -15px, 0);
-// 		}
+  function getRule() {
 
-// 		90% {
-// 			-webkit-transform: translate3d(0,-4px,0);
-// 			transform: translate3d(0,-4px,0);
-// 		}
-// 	}', this.stylesheet.rules.length);
-//     } catch (e) {};
+      var rule;
+
+      var ss = document.styleSheets;
+
+      for (var i = 0; i < ss.length; ++i) {
+
+          // loop through all the rules!
+
+          for (var x = 0; x < ss[i].cssRules.length; ++x) {
+
+              rule = ss[i].cssRules[x];
+
+              if (rule.name == enterPiece && rule.type
+
+                      == CSSRule.KEYFRAMES_RULE) {
+
+                  cssRule = rule;
+
+              }
+
+          }
+
+      }
+
+  }
