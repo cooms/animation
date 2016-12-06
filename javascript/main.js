43919df7867a135fc9bcb1ac9d3df2a7cdcee33f
cooms <thcoomer@hotmail.com>
2016-12-06 18:39:36 +1300

    fixed merge conflicts

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,8 @@
 // javascript
-function bounce() {
-	document.getElementById('ball').style.backgroundColor = "blue";
-	var rule;
-	var ss = document.styleSheets;
-	for (var i = 0; i < ss.length; ++i) {
-		// loop through all the rules!
-		for (var x = 0; x < ss[i].cssRules.length; ++x) {
-			rule = ss[i].cssRules[x];
-			if (rule.name == bounce && rule.type == CSSRule.KEYFRAMES_RULE) {
-				cssRule = rule;
-			}
-		}
-	}
-}
 
 var ball = document.getElementById('ball')
 
 ball.addEventListener("click", function(){
-	ball.style.animation = "bounce 1s"
+	ball.style.animation = "bounce 2s"
+	ball.style.backgroundColor = "blue"
 })
